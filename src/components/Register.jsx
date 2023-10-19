4b54806b319056f77acb95767f839248cd3275b0
M A K <45846310+EtoYaMak@users.noreply.github.com>
2023-10-19 17:55:43 +0100

    validation paragraphs added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import { BiCheck, BiX, BiInfoCircle } from "react-icons/bi";
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 function Register() {
   return (
-    <div className="hero mt-10 bg-none">
-      <div className="hero-content flex-col w-full xl:flex-row-reverse xl:w-fit ">
+    <div className="hero xl:mt-[10vh] mt-[5vh] bg-none">
+      <div className="hero-content flex-col w-full xl:flex-row xl:w-fit xl:gap-32 gap-10 ">
         <div className="text-center xl:text-left text-[#DFBB8E] select-none">
           <h1 className="text-5xl font-bold">Register</h1>
-          <p className="py-6">Test</p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-[#B53C66]">
           <form className="card-body">
@@ -22,6 +25,14 @@ function Register() {
                 className="input input-ghost bg-[#291E37] focus:bg-[#DFBB8E] focus:text-[#291E37] text-[#EFF4E9] placeholder:text-[#DFBB8E]"
                 required
               />
+              <p className="flex text-sm text-white py-2 select-none">
+                <BiInfoCircle size={18} className="text-[#291E37] mr-1 w-8" />
+                4 to 24 characters.
+                <br />
+                Must begin with a letter.
+                <br />
+                Letters, numbers, underscores, hyphens allowed.
+              </p>
             </div>
             <div className="form-control">
               <label className="label">
@@ -35,6 +46,15 @@ function Register() {
                 className="input input-ghost bg-[#291E37] focus:bg-[#DFBB8E] focus:text-[#291E37] text-[#EFF4E9] placeholder:text-[#DFBB8E]"
                 required
               />
+              <p className="flex text-sm text-white py-2 select-none">
+                <BiInfoCircle size={18} className="text-[#291E37] mr-1 w-8" />
+                8 to 24 characters.
+                <br />
+                Must include uppercase and lowercase letters, a number and a
+                special character.
+                <br />
+                Allowed special characters: ! @ # $ %
+              </p>
             </div>
             <div className="form-control">
               <label className="label">
@@ -48,6 +68,10 @@ function Register() {
                 className="input input-ghost bg-[#291E37] focus:bg-[#DFBB8E] focus:text-[#291E37] text-[#EFF4E9] placeholder:text-[#DFBB8E]"
                 required
               />
+              <p className="flex text-sm text-white py-2 select-none">
+                <BiInfoCircle size={18} className="text-[#291E37] mr-1 w-8" />
+                Must match the first password input field.
+              </p>
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-ghost bg-[#EFF4E9] hover:bg-[#DFBB8E] text-[#291E37] hover:text-white text-md ">
